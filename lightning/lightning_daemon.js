c803ac2a621ac710a439163c249972139922c439
Austin <austin@blockseer.com>
2018-07-10 10:22:54 -0700

    Rename env variables

--- lightning/lightning_daemon.js
+++ lightning/lightning_daemon.js
@@ -1,12 +1,12 @@
 const {lightningDaemon} = require('ln-service');
 
-const {SSS_LND_GRPC_HOST_BTC} = process.env;
-const {SSS_LND_MACAROON_BTC} = process.env;
-const {SSS_LND_TLS_CERT_BTC} = process.env;
+const {SSS_BTC_LND_GRPC_HOST} = process.env;
+const {SSS_BTC_LND_MACAROON} = process.env;
+const {SSS_BTC_LND_TLS_CERT} = process.env;
 
-const {SSS_LND_GRPC_HOST_LTC} = process.env;
-const {SSS_LND_MACAROON_LTC} = process.env;
-const {SSS_LND_TLS_CERT_LTC} = process.env;
+const {SSS_LTC_LND_GRPC_HOST} = process.env;
+const {SSS_LTC_LND_MACAROON} = process.env;
+const {SSS_LTC_LND_TLS_CERT} = process.env;
 
 /** Get the Lightning Network Daemon connection
 
@@ -22,15 +22,15 @@ module.exports = ({network}) => {
   try {
     if (network === "testnet") {
       return lightningDaemon({
-        cert: SSS_LND_TLS_CERT_BTC,
-        host: SSS_LND_GRPC_HOST_BTC,
-        macaroon: SSS_LND_MACAROON_BTC,}
+        cert: SSS_BTC_LND_TLS_CERT,
+        host: SSS_BTC_LND_GRPC_HOST,
+        macaroon: SSS_BTC_LND_MACAROON,}
       );
     } else if (network === "ltctestnet"){
       return lightningDaemon({
-        cert: SSS_LND_TLS_CERT_LTC,
-        host: SSS_LND_GRPC_HOST_LTC,
-        macaroon: SSS_LND_MACAROON_LTC,}
+        cert: SSS_LTC_LND_TLS_CERT,
+        host: SSS_LTC_LND_GRPC_HOST,
+        macaroon: SSS_LTC_LND_MACAROON,}
       );
     }
   } catch (e) {
