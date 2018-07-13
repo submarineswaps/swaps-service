cbffecde152b3aede0b8565ceabe72f9d2cf2c81
Austin <austin@blockseer.com>
2018-07-13 11:53:36 -0700

    Debug commit

--- test/macros/spawn_chain_daemon.js
+++ test/macros/spawn_chain_daemon.js
@@ -6,20 +6,20 @@ const bitcoincoreTypeDaemon = require('./bitcoincore_type_daemon')
 const btcsuiteTypeDaemon = require('./btcsuite_type_daemon');
 /** Spawn a chain daemon for testing on regtest
 
-  This method will also listen for uncaught exceptions and stop the daemon
-  before the process dies.
+ This method will also listen for uncaught exceptions and stop the daemon
+ before the process dies.
 
-  {
-    daemon: <Daemon Type String>
-    mining_public_key: <Mining Public Key Hex String>
-    network: <Network Name String>
-  }
+ {
+   daemon: <Daemon Type String>
+   mining_public_key: <Mining Public Key Hex String>
+   network: <Network Name String>
+ }
 
-  @returns via cbk
-  {
-    is_ready: <Chain Daemon is Ready Bool>
-  }
-*/
+ @returns via cbk
+ {
+   is_ready: <Chain Daemon is Ready Bool>
+ }
+ */
 module.exports = (args, cbk) => {
   if (!args.daemon) {
     return cbk([400, 'ExpectedDaemonTypeToSpawn']);
@@ -37,15 +37,25 @@ module.exports = (args, cbk) => {
   case 'bcoin':
     daemon = bcoinTypeDaemon({dir, network: args.network}, cbk);
     break;
+  case 'btcdbackend':
+    console.log("spawning btcd for lightning backend");
+    daemon = btcsuiteTypeDaemon({
+        dir,
+        daemon: args.daemon,
+        noMine: true,
+        tls: true,
+        network: args.network,
+      },
+      cbk);
+    break;
   case 'btcd':
   case 'ltcd':
-    console.log("spawning suite daemon")
+    console.log("spawning suite daemon");
     daemon = btcsuiteTypeDaemon({
         dir,
         daemon: args.daemon,
         mining_public_key: args.mining_public_key,
         network: args.network,
-        lnd: args.lnd,
       },
       cbk);
     break;
@@ -62,11 +72,11 @@ module.exports = (args, cbk) => {
   }
 
   daemon.stderr.on('data', data => {
-    if (/mandatory.script.verify.flag/gim.test(data+'')) {
+    if (/mandatory.script.verify.flag/gim.test(data + '')) {
       return;
     }
 
-    if (/txn.already.in.mempool/gim.test(data+'')) {
+    if (/txn.already.in.mempool/gim.test(data + '')) {
       return;
     }
 
