5c286830d4a0196a378328cd5265d28871160295
Austin <austin@blockseer.com>
2018-07-16 10:18:39 -0700

    Refactor to move methods to apiMethods, constants to api_constants

--- routers/api.js
+++ routers/api.js
@@ -3,84 +3,67 @@ const {Router} = require('express');
 
 const {checkSwapStatus} = require('./../service');
 const {createSwap} = require('./../service');
-const {findSwapOutpoint} = require('./../service');
-const {getAddressDetails} = require('./../service');
-const {getExchangeRates} = require('./../service');
-const {getInvoiceDetails} = require('./../service');
 const {returnJson} = require('./../async-util');
 
-const cache = 'redis';
-const swapNetworks = ['bchtestnet', 'ltctestnet', 'testnet'];
+const apiConstants = require('./../chain/conf/api_constants.json');
+
+// const cache = 'redis';
+// const apiConstants.swapNetworks = ['bchtestnet', 'ltctestnet', 'testnet'];
+
+const {reqGetAddressDetails} = require('./apiMethods');
+const {reqGetExchangeRates} = require('./apiMethods');
+const {reqGetInvoiceDetails} = require('./apiMethods');
+const {reqFindSwapOutpoint} = require('./apiMethods');
+const {reqCreateSwap} = require('./apiMethods');
+const {reqCheckSwapStatus} = require('./apiMethods');
 
 /** Make an api router
 
-  {
-    log: <Log Function>
-  }
+ {
+   log: <Log Function>
+ }
 
-  @returns
-  <Router Object>
-*/
+ @returns
+   <Router Object>
+ */
+
+
 module.exports = ({log}) => {
   const router = Router({caseSensitive: true});
-
   router.use(bodyParser.json());
 
   // GET details about an address
-  router.get('/address_details/:network/:address', ({params}, res) => {
-    const {address} = params;
-
-    return getAddressDetails({address, network}, returnJson({log, res}));
-  });
+  router.get('/address_details/:network/:address', (req, res) => reqGetAddressDetails(req, res, log));
 
   // GET exchange rate information
-  router.get('/exchange_rates/', ({}, res) => {
-    return getExchangeRates({
-      cache,
-      networks: swapNetworks,
-    },
-    returnJson({log, res}));
-  });
+  router.get('/exchange_rates/', reqGetExchangeRates);
 
   // GET details about an invoice
-  router.get('/invoice_details/:network/:invoice', ({params}, res) => {
-    return getInvoiceDetails({
-      cache,
-      invoice: params.invoice,
-      network: params.network,
-    },
-    returnJson({log, res}));
-  });
+  router.get('/invoice_details/:network/:invoice', reqGetInvoiceDetails);
 
   // POST a swap output find details request
-  router.post('/swap_outputs/', ({body}, res) => {
-    return findSwapOutpoint({
-      network: body.network,
-      redeem_script: body.redeem_script,
-    },
-    returnJson({log, res}));
-  });
+  router.post('/swap_outputs/', (req, res) => {reqFindSwapOutpoint(req, res, log)});
 
   // POST a new swap
   router.post('/swaps/', ({body}, res) => {
     return createSwap({
-      cache,
-      invoice: body.invoice,
-      network: body.network,
-      refund: body.refund,
-    },
-    returnJson({log, res}));
+        cache: apiConstants.cache,
+        invoice: body.invoice,
+        network: body.network,
+        refund: body.refund,
+      },
+      returnJson({log, res}));
   });
 
   // POST a swap check request
   router.post('/swaps/check', ({body, params}, res) => {
     return checkSwapStatus({
-      cache,
-      invoice: body.invoice,
-      network: body.network,
-      script: body.redeem_script,
-    },
-    returnJson({log, res}));
+        cache: apiConstants.cache,
+        invoice: body.invoice,
+        network: body.network,
+        script: body.redeem_script,
+      },
+      returnJson({log, res}));
   });
 
   return router;
