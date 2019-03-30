3552ee37e82e6007a6df7c8e31cf281eabc95eb0
Alex Bosworth <alex.bosworth@gmail.com>
2019-03-30 15:03:42 -0700

    restart on high mem usage

--- server.js
+++ server.js
@@ -17,6 +17,7 @@ const cache = 'redis';
 const isProduction = NODE_ENV === 'production';
 const {log} = console;
 const logOnErr = err => !!err ? console.log(err) : null;
+const nodeMemLimit = 475;
 const port = PORT || SSS_PORT || 9889;
 const scannersStartDelay = 1000 * 10;
 
@@ -44,3 +45,8 @@ if (!isProduction) {
   walnut.check(require('./package'));
 }
 
+setInterval(() => {
+  if (Math.round(process.memoryUsage().rss / 1024 / 1024) > nodeMemLimit) {
+    process.exit();
+  }
+}, 5000);
